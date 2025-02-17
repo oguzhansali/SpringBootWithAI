8354016530ca02ee594c2434549a9fbf46b315b4
Oguzhan Sali <salioguzhan.61@gmail.com>
2025-02-17 11:24:45 +0300

    Project Files

---
+++ src/main/java/AI/With/Spring/Bootcom/example/Spring/Boot/with/AI/SpringBootWithAiApplication.java
@@ -0,0 +1,13 @@
+package AI.With.Spring.Bootcom.example.Spring.Boot.with.AI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/AI/With/Spring/Bootcom/example/Spring/Boot/with/AI/SpringBootWithAiApplicationTests.java
@@ -0,0 +1,13 @@
+package AI.With.Spring.Bootcom.example.Spring.Boot.with.AI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
